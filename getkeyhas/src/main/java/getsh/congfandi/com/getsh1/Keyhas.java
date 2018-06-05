e9f8a90ab59db0d9451b8098913bb43948557e70
Cong Fandi <congfandi@gmail.com>
2018-06-05 10:32:04 +0700

    init

---
+++ getkeyhas/src/main/java/getsh/congfandi/com/getsh1/Keyhas.java
@@ -0,0 +1,30 @@
+package getsh.congfandi.com.getsh1;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Keyhas {
+    public static void getKey(Context context) {
+        try {
+
+            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : packageInfo.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHas", "onCreate: " + Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
